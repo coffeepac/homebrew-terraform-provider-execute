b8d2033aedb09761d78b7498934e7a10107e5c16
Marat G <maratoid@gmail.com>
2015-08-17 19:53:55 -0700

    switch to using a tarball

--- terraform-provider-execute.rb
+++ terraform-provider-execute.rb
@@ -1,9 +1,9 @@
-class TerraformProviderExecuteDarwinAmd < Formula
+class TerraformProviderExecute < Formula
   desc "Execute arbitrary commands on Terraform create and destroy"
   homepage "https://github.com/Samsung-AG/terraform-provider-execute"
-  url "https://github.com/Samsung-AG/terraform-provider-execute/releases/download/v0.0.1/terraform-provider-execute_darwin_amd64"
+  url "https://github.com/Samsung-AG/terraform-provider-execute/releases/download/v0.0.1/terraform-provider-execute_darwin_amd64.tar.gz"
   version "0.0.1"
-  sha256 "60bc4b9c693dfb815db61d0599a3644ca1b8c191d6986b63da8740c2b56fc76b"
+  sha256 "e2b836e72a7361549200b24d43df7da9eb3700a84a49de7c2acc4f3a2718ad58"
 
   def install
     bin.install "terraform-provider-execute"
