c5df1c670da34d9135d0ebbab32076c05f0a9a02
Marat G <maratoid@gmail.com>
2015-08-21 13:28:01 -0700

    update to 0.0.2

--- terraform-provider-execute.rb
+++ terraform-provider-execute.rb
@@ -1,9 +1,9 @@
 class TerraformProviderExecute < Formula
   desc "Execute arbitrary commands on Terraform create and destroy"
   homepage "https://github.com/Samsung-AG/terraform-provider-execute"
-  url "https://github.com/Samsung-AG/terraform-provider-execute/releases/download/v0.0.1/terraform-provider-execute_darwin_amd64.tar.gz"
-  version "0.0.1"
-  sha256 "e2b836e72a7361549200b24d43df7da9eb3700a84a49de7c2acc4f3a2718ad58"
+  url "https://github.com/Samsung-AG/terraform-provider-execute/releases/download/v0.0.2/terraform-provider-execute_darwin_amd64.tar.gz"
+  version "0.0.2"
+  sha256 "b38919d9c2e139dee315855a77d01e5e42452539eb2fc89c47bbe21c1d9c5133"
 
   def install
     bin.install "terraform-provider-execute"
